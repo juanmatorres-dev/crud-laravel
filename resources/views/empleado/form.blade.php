9523c4f4f5707e5d02d629aa6a8fc527150709e4
Juan Manuel <veinte2000jtm23@gmail.com>
2022-01-10 18:22:49 +0100

    Mejora de formulario reutilizable (crear y editar empleados)
    Ajustado el formulario para que se pueda reutilizar para crear y editar empleados (se ha hecho comprobando que la información exista)

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -2,19 +2,21 @@
 
 
 <label for="Nombre">Nombre:</label>
-<input type="text" name="Nombre" value="{{ $empleado->Nombre }}" id="Nombre">
+<input type="text" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:'' }}" id="Nombre">
 <br />
 <label for="Apellido1">Apellido1:</label>
-<input type="text" name="Apellido1" value="{{ $empleado->Apellido1 }}" id="Apellido1">
+<input type="text" name="Apellido1" value="{{ isset($empleado->Apellido1)?$empleado->Apellido1:'' }}" id="Apellido1">
 <br />
 <label for="Apellido2">Apellido2:</label>
-<input type="text" name="Apellido2" value="{{ $empleado->Apellido2 }}" id="Apellido2">
+<input type="text" name="Apellido2" value="{{ isset($empleado->Apellido2)?$empleado->Apellido2:'' }}" id="Apellido2">
 <br />
 <label for="Email">Email:</label>
-<input type="email" name="Email" value="{{ $empleado->Email }}" id="Email">
+<input type="email" name="Email" value="{{ isset($empleado->Email)?$empleado->Email:'' }}" id="Email"> <!-- isset($empleado->Email)?$empleado->Email:'' Valida si existe el dato, si existe lo imprime, sino lo imprime vacío  -->
 <br />
 <label for="Foto">Foto:</label>
+@if(isset($empleado->Foto))
 <img src="{{ asset('storage').'/'.$empleado->Foto }}" width="95px" alt="">
+@endif
 <input type="file" name="Foto" value="" id="Foto">
 <br />
 <input type="submit" value="Guardar datos" id="Guardar datos">
